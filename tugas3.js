a71ef82c7c25262c3913076814ccea5a350de8d5
sitimarni <124442828+sitimarni@users.noreply.github.com>
2023-10-20 20:40:05 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,14 @@
+// Objek Restaurant
+const restaurant = {
+    name: "Geprek Koboy",
+    city: "Citymall",
+    favoriteDrink: "Lemon Tea",
+    favoriteFood: "Ayam Mozzarella",
+    isVegan: false
+  };
+  
+  console.log("Restaurant Name:", restaurant.name);
+  console.log("City:", restaurant.city);
+  console.log("Favorite Drink:", restaurant.favoriteDrink);
+  console.log("Favorite Food:", restaurant.favoriteFood);
+  console.log("Is Vegan:", restaurant.isVegan);
---
+++ tugasarray.js
@@ -0,0 +1,8 @@
+// NO 2 Varriable nama restaurant yang bertipe array
+const restaurant = ["nama restaurant :Masson", "city : Sukabumi", "Favorite Drink : Lemon tea"]
+const[firstName, secondCity, thirdFavoriteDrink, fourthFavoriteFood, fiveisVegan] = restaurant;
+console.log(firstName);
+console.log(secondCity);
+console.log(thirdFavoriteDrink);
+console.log(fourthFavoriteFood);
+console.log(fiveisVegan)
